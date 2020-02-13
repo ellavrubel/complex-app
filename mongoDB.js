02f10db34a075bb216871ae70acdddae54c438ee
ellavrubel <webella@protonmail.com>
2020-02-13 18:38:10 +0300

    env

--- mongoDB.js
+++ mongoDB.js
@@ -1,12 +1,13 @@
 
+            const dotenv = require('dotenv');
+            dotenv.config();
+
             const mongodb = require('mongodb');
 
-            const connectionString = 'mongodb+srv://todoAppUser:rehnrkfq@firstapp-khioh.mongodb.net/complex-app?retryWrites=true&w=majority';
-
-            mongodb.connect(connectionString, {useNewUrlparser: true, useUnifiedTopology: true}, function (err, client) {
+            mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlparser: true, useUnifiedTopology: true}, function (err, client) {
 
                 module.exports = client.db();
                 const app = require('./app');
-                app.listen(2000);
+                app.listen(process.env.PORT);
 
             });
